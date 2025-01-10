56d2ba63e0229563d1282fb881adac986132ab0d
石戸莞楽 <119462728+Kanra-Ishido@users.noreply.github.com>
2025-01-10 19:41:22 +0900

    Revert "フィボナッチ数列用のユースケースclassを作成"

--- application/usecase/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-from usecase.fibonacci_series import FibonacciSeriesUsecase
-
-Usecase = {
-    "FibonacciSeriesUsecase": FibonacciSeriesUsecase
-}
