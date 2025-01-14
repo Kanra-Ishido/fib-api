d3fcb71b43243defbcfaf494862f1b3aff069613
Kanra_Ishido <s2222002@stu.musashino-u.ac.jp>
2025-01-14 17:37:02 +0900

    serverlessフレームワークでlambdaにデプロイ

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from fastapi import FastAPI
+from mangum import Mangum
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from routes import fibonacci_series_route as fibonacci_series_route
@@ -8,3 +9,5 @@ from routes import fibonacci_series_route as fibonacci_series_route
 app = FastAPI()
 
 app.include_router(fibonacci_series_route.app, prefix='/fibonacci_series')
+
+handler = Mangum(app)
